9ff3aa1f9ac73dd26b3744c5eddadc74648a5e25
xli <170154@gmail.com>
2016-08-21 21:51:35 -0400

    change DB connection timezone

--- sentview/shared.py
+++ sentview/shared.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sentview.config import config
 
-engine = create_engine(config.DB_URI, connect_args={'connect_timeout': 10}, pool_recycle=300)
+engine = create_engine(
+	config.DB_URI, 
+	connect_args={'connect_timeout': 10, 'options': '-c timezone=utc'},
+	pool_recycle=300
+)
 dbsession = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
