08565962675f57f1376d880bcb39dc9c84683651
xli <170154@gmail.com>
2016-10-01 20:47:09 -0400

    Make debug setting configurable from env. var.

--- runserver.py
+++ runserver.py
@@ -1,8 +1,12 @@
+import os
+
 import eventlet
 eventlet.monkey_patch()
+
 from sentview import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-	socketio.run(app, debug=True)
+	debug = os.environ.get('SENTVIEW_ENV') != 'PROD'
+	socketio.run(app, debug=debug)
